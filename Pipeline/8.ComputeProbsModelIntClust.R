0e7a7b71fda5b969bdf3409a0c6598c682eb70fe
Oscar Rueda <rueda.om@gmail.com>
2019-01-22 15:59:18 +0000

    Add files via upload

--- Pipeline/8.ComputeProbsModelIntClust.R
+++ Pipeline/8.ComputeProbsModelIntClust.R
@@ -1,17 +1,30 @@
 rm(list=ls())
-id <- as.numeric(Sys.getenv("LSB_JOBINDEX"))
+id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+print(id)
 library(mstate)
 library(brcarepred)
-load("ICM.RData")
-fitted <- msfit(m, newdata=newdata, trans=tra)
+load("../Models/ICM.RData")
 
 ## Predictions
 
 timepoints <- seq(from=0, to=20, by=0.25)
 pt <- list()
-pt[['DR']] <- getProbsDR(m, group=id, newdata, timepoints=timepoints)
+newdata.DR <- newdata
+newdata.DR$AGE.DR[which(newdata.DR$strata %in% seq(from=9,by=9, length=11))] <-
+    newdata.DR$AGE.DR[which(newdata.DR$strata %in% seq(from=9,by=9, length=11))] +
+        newdata.DR$TLastSurgery[which(newdata.DR$strata %in%
+                                       seq(from=8, by=9, length=11))]
+pt[['DR']] <- getProbsDR(m, group=id, newdata.DR, timepoints=timepoints)
 
-pt[['LR']] <- getProbsLR(m, group=id, newdata, timepoints=timepoints, compact=F)
+newdata.LR <- newdata
+newdata.LR$AGE.LR[which(newdata.LR$strata %in% seq(from=7, by=9, length=11))] <-
+newdata.LR$AGE.LR[which(newdata.LR$strata %in% seq(from=7, by=9, length=11))] +
+    newdata.LR$TLastSurgery[which(newdata.LR$strata %in%
+                                   seq(from=6, by=9, length=11))]
+newdata.LR$AGE.DR[which(newdata.LR$strata %in% seq(from=9,by=9, length=11))] <-
+newdata.LR$AGE.LR[which(newdata.LR$strata %in% seq(from=7, by=9, length=11))]
+
+pt[['LR']] <- getProbsLR(m, group=id, newdata.LR, timepoints=timepoints, compact=F)
 
 timepoints <- c(seq(from=0, to=15, by=0.25), 16:20)
 system.time(pt[['S']] <- getProbsS(m, group=id, newdata, timepoints=timepoints, compact=FALSE))
